c99fecca53cac505c745faca135f99636a443af7
lew0205 <lew0205@naver.com>
2021-10-28 16:55:43 +0900

    211028

--- 035_Operator2/Program.cs
+++ 035_Operator2/Program.cs
@@ -10,6 +10,27 @@ namespace _035_Operator2
     {
         static void Main(string[] args)
         {
+            int a = 10, b = 3;
+            float c = 123.45f, d = 3.1456f;
+
+            Console.WriteLine("a: {0}", a);
+            Console.WriteLine("b: {0}", b);
+            Console.WriteLine("c: {0}", c);
+            Console.WriteLine("d: {0}", d);
+
+            Console.WriteLine("\n정수의 계산");
+            Console.WriteLine("a + b = {0}", a + b);
+            Console.WriteLine("a - b = {0}", a - b);
+            Console.WriteLine("a * b = {0}", a * b);
+            Console.WriteLine("a / b = {0}", a / b);
+            Console.WriteLine("a % b = {0}", a % b);
+
+            Console.WriteLine("\n실수의 계산");
+            Console.WriteLine("c + d = {0}", c + d);
+            Console.WriteLine("c - d = {0}", c - d);
+            Console.WriteLine("c * d = {0}", c * d);
+            Console.WriteLine("c / d = {0}", c / d);
+            Console.WriteLine("c % d = {0}", c % d);
         }
     }
 }
