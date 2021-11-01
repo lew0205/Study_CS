6432548840d8fd7778372148f5d12aff6735da18
lew0205 <lew0205@naver.com>
2021-11-01 22:28:57 +0900

    211101

--- 037_Operator4/Program.cs
+++ 037_Operator4/Program.cs
@@ -10,7 +10,27 @@ namespace _037_Operator4
     {
         static void Main(string[] args)
         {
+            bool result;
+            int a = 100;
+            int b = 1000;
 
+            result = (a == b);
+            Console.WriteLine("(a == b): {0}", result);
+
+            result = (a != b);
+            Console.WriteLine("(a != b): {0}", result);
+            
+            result = (a > b);
+            Console.WriteLine("(a > b): {0}", result);
+
+            result = (a < b);
+            Console.WriteLine("(a < b): {0}", result);
+            
+            result = (a >= b);
+            Console.WriteLine("(a >= b): {0}", result);
+            
+            result = (a <= b);
+            Console.WriteLine("(a <= b): {0}", result);
         }
     }
 }
