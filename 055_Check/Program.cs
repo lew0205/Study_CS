521879a735fc3ab1ab4440cd216295e7f6cd9aa6
lew0205 <lew0205@naver.com>
2021-11-03 20:29:21 +0900

    211103

---
+++ 055_Check/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _055_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = 0, b = 0, sum = 0;
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}: 다음 두 수의 합은 몇?(총 5문제)",i);
+                a = rnd.Next(1, 100);
+                b = rnd.Next(1, 100);
+                sum = a + b;
+                Console.Write("{0} + {1} = ",a,b);
+                int ans = int.Parse(Console.ReadLine());
+                if (ans == sum)
+                {
+                    Console.WriteLine("== 정답 ==");
+                }
+                else
+                {
+                    Console.WriteLine("오답(정답은: {0})", sum);
+                }
+            }
+        }
+    }
+}
---
+++ 056_Check/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _056_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 100);
+            int cnt = 0;
+
+            for(; ; )
+            {
+                cnt++;
+                Console.WriteLine("0 ~ 99 사이 어떤 숫자일까요(단, 0은 나가기)");
+                int ans = int.Parse(Console.ReadLine());
+
+                if (ans == 0)
+                {
+                    break;
+                }
+                if (ans > num)
+                {
+                    Console.WriteLine("입력한 수는 커요");
+                }
+                else if(ans < num)
+                {
+                    Console.WriteLine("입력한 수는 작아요");
+                }
+                else
+                {
+                    Console.WriteLine("=== 정답 입니다. ===");
+                    Console.WriteLine("총 {0}번 시도", cnt);
+                    break;
+                }
+            }
+        }
+    }
+}
