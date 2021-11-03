4753e598fbdf8a7dab0bc761dc59eda3881e9212
lew0205 <lew0205@naver.com>
2021-11-03 14:19:04 +0900

    211103

---
+++ 043_Operator_Branch2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _043_Operator_Branch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("정수를 입력하세요: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine("입력한 수는 양수!!");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("입력한 수는 음수!!");
+            }
+            else
+            {
+                Console.WriteLine("입력한 수는 0이다");
+            }
+        }
+    }
+}
