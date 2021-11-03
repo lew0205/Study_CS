3cba7aa69fc20ce6ddb295229625fc7ac6dbb533
lew0205 <lew0205@naver.com>
2021-11-03 19:35:34 +0900

    211103

---
+++ 051_Operator_while(true)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _051_Operator_while_true_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0;
+            while (true)  // for(;;)
+            {
+                num++;
+                Console.WriteLine("while(true) {0}",num);
+            }
+        }
+    }
+}
