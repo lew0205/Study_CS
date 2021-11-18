b2ec7500ecae7fa42315142bbd85b57a29cc951c
lew0205 <lew0205@naver.com>
2021-11-18 22:48:51 +0900

    211118

---
+++ 067_Check/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _067_Check
+{
+    class Program
+    {
+        static void Start()
+        {
+            Console.WriteLine("성적 프로그램 - Method");
+        }
+
+        static void Input(ref int kor, ref int mat, ref int eng)
+        {
+            Console.Write("국어 성적 입력(정수)? ");
+            kor = int.Parse(Console.ReadLine());
+            Console.Write("수학 성적 입력(정수)? ");
+            mat = int.Parse(Console.ReadLine());
+            Console.Write("국어 성적 입력(정수)? ");
+            eng = int.Parse(Console.ReadLine());
+        }
+
+        static int Total(int kor,int mat,int eng)
+        {
+            return (kor + mat + eng);
+        }
+
+        static void Average(int total, out float average)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            int kor = 0;
+            int eng = 0;
+            int mat = 0;
+            int total;
+            float average;
+
+            
+        }
+    }
+}
